63a12ab5062dbf0afc5d9d3f1f497b4071a59744
Said Sadykzhanov <saidsadykzhanov1@gmail.com>
2026-06-30 17:15:10 +0600

    add gallery component

--- src/pages/about/gallery/Gallery.jsx
+++ src/pages/about/gallery/Gallery.jsx
@@ -6,7 +6,9 @@ function Gallery() {
         <section className="gallery">
             <div className="container">
                 <div className="gallery__box">
-                    
+                    <div className="gallery__card"></div>
+                    <div className="gallery__card"></div>
+                    <div className="gallery__card"></div>
                 </div>
             </div>
         </section>
