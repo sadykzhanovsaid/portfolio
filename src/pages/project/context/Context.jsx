56e2580e2e171a3a3485c6f43a78d4e2e8b415ff
Said Sadykzhanov <saidsadykzhanov1@gmail.com>
2026-07-09 23:20:44 +0600

    migrate images from PNG to WebP

--- src/pages/project/context/Context.jsx
+++ src/pages/project/context/Context.jsx
@@ -9,24 +9,24 @@ function Context({context, projectImages}) {
                     <p className="context__title"><span>Context</span> <br/> {context[0]}</p>
 
                     <div className="context__image-desktop">
-                        <img src={`/screens/zara/${projectImages[0]}.png`} alt="project image"/>
+                        <img src={`/screens/zara/${projectImages[0]}.webp`} alt="project image"/>
                     </div>
 
                     <p className="context__title">{context[1]}</p>
 
                     <div className="context__image-desktop">
-                        <img src={`/screens/zara/${projectImages[1]}.png`} alt="project image"/>
+                        <img src={`/screens/zara/${projectImages[1]}.webp`} alt="project image"/>
                     </div>
 
                     <p className="context__title">{context[2]}</p>
 
                     <div className="context__images-mobile">
                         <div className="context__image-mobile">
-                            <img src={`/screens/zara/${projectImages[2]}.png`} alt="project image"/>
+                            <img src={`/screens/zara/${projectImages[2]}.webp`} alt="project image"/>
                         </div>
 
                         <div className="context__image-mobile">
-                            <img src={`/screens/zara/${projectImages[3]}.png`} alt="project image"/>
+                            <img src={`/screens/zara/${projectImages[3]}.webp`} alt="project image"/>
                         </div>
                     </div>
                 </div>
