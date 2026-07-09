9d54cd142d86b28fb60ae13cb43633408c96cdbf
Said Sadykzhanov <saidsadykzhanov1@gmail.com>
2026-07-09 23:06:56 +0600

    add context component

--- src/pages/project/context/Context.jsx
+++ src/pages/project/context/Context.jsx
@@ -7,8 +7,28 @@ function Context({context, projectImages}) {
             <div className="container">
                 <div className="context__box">
                     <p className="context__title"><span>Context</span> <br/> {context[0]}</p>
+
+                    <div className="context__image-desktop">
+                        <img src={`/screens/zara/${projectImages[0]}.png`} alt="project image"/>
+                    </div>
+
                     <p className="context__title">{context[1]}</p>
+
+                    <div className="context__image-desktop">
+                        <img src={`/screens/zara/${projectImages[1]}.png`} alt="project image"/>
+                    </div>
+
                     <p className="context__title">{context[2]}</p>
+
+                    <div className="context__images-mobile">
+                        <div className="context__image-mobile">
+                            <img src={`/screens/zara/${projectImages[2]}.png`} alt="project image"/>
+                        </div>
+
+                        <div className="context__image-mobile">
+                            <img src={`/screens/zara/${projectImages[3]}.png`} alt="project image"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
